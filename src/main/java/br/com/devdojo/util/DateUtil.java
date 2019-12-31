9f7bca247b9132a91ef4e3294a2398fb9676ec35
Roger Joel <roger.joel.guillermo@outlook.com>
2019-12-31 20:38:19 -0300

    Base Project

--- src/main/java/br/com/devdojo/util/DateUtil.java
+++ src/main/java/br/com/devdojo/util/DateUtil.java
@@ -1,4 +1,14 @@
 package br.com.devdojo.util;
 
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
 public class DateUtil {
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
 }
